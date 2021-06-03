e171a48642241232732b30bc74f2a609b0008968
Jackkyy <85114983+jackkkkkkyyy@users.noreply.github.com>
2021-06-03 16:50:19 +0700

    Hai stah

---
+++ Didin/sindiran.js
@@ -0,0 +1,104 @@
+[
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *Percuma fitur banyak tapi pada mati*"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *Udh tau cepu kenapa masih di kasih? Akal nya dimana?*"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *Mastah memang beda*"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *wih cepu, kapan ya bisa gitu?*"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *udh tau cepu, masih aja di anggap teman*"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *Demi konten, up youtube dulu kita boy*"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *Keren, kapan ya bisa sebar fitur?*"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *Copy dulu baru paste*"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *Gak ada referensi ya? Pantes maling terus*"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *Baca dulu, pahami terus copy*"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *Makanya copy yang bener*"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *keren, kapan ya bisa jago*"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *Jangan di suapin terus, Kapan mandiri nya*"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *Makanya copy yang bener*"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *Pahami -> resapi -> minta case*"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *Jika pelit lanjutkan*"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "Sindiran": "*ANBOT-D* ➣ *Gua bukan dukun, Kalo ga di kasih tunjuk fiturnya ga bisa kasih solusi dodol*"
+    }
+}
+]
