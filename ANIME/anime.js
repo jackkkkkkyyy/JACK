5507aab5660e4d9efc588623b9fb8ee60aad9cc6
Jackkyy <85114983+jackkkkkkyyy@users.noreply.github.com>
2021-06-03 16:40:05 +0700

    Add files via upload

---
+++ ANIME/anime.js
@@ -0,0 +1,167 @@
+[
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.imgur.com/rNEUT4Z.jpg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.waifu.pics/U0xGNIS.jpg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.waifu.pics/zxSlO-7.png"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.imgur.com/X6OpdJn.jpg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.imgur.com/qYcpJr7.jpg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https:\/\/i.pinimg.com\/originals\/dc\/28\/1c\/dc281c15573fd1e58d989a6d602510f3.jpg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https:\/\/i.pinimg.com\/originals\/71\/68\/70\/716870dde49840093900257d7b0603c9.jpg"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://thicc.mywaifulist.moe/waifus/15400/905182ba723b90528448dfd43e066567c81e4304cc097a96c388221c42f99e4f_thumb.jpeg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.waifu.pics/fPCPJBI.jpg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.imgur.com/ol2Oj8R.jpg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.waifu.pics/Owt_E3B.jpg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.waifu.pics/anKsYF2.png"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.waifu.pics/BiP6Bl4.png"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.imgur.com/w5dY397.png"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.waifu.pics/RREE3af.png"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://thicc.mywaifulist.moe/waifus/8634/9d686727fb5df8760f4ed0db1bf67f51fdaf82553fa937036e94a319adf10d86_thumb.png"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.waifu.pics/CmAsGKo.jpg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.waifu.pics/ubt7vL8.jpg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.waifu.pics/P817hp4.jpg"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.waifu.pics/Dv7ns4N.jpg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.waifu.pics/Vmq45v5.jpg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.waifu.pics/1a-JbbZ.jpg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": ""
+    }
+},
+{"result": {
+  "ranime": "https://i.imgur.com/nSwNlyf.jpg"}
+  },
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.imgur.com/iLBiiLo.jpg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https:\/\/i.pinimg.com\/originals\/60\/11\/81\/6011814d0b116353fbf35a2ec9f775a9.jpg"
+    }
+},
+{
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.imgur.com/E1JjyQb.jpg"
+    }
+},
+  {
+    "result": {
+        "Created": "ANBOT-D <DIDIN>",
+        "ranime": "https://i.waifu.pics/5PD0rm~.jpg"
+    }
+}
+]
